b2a281b258a2abe2bdee680dae1c9d9b2b97974e
Gizdulock <gizdulock@example.com>
2023-06-26 09:18:12 +0000

    Initial commit

---
+++ music_generator.py
@@ -0,0 +1,11 @@
+import magenta
+from magenta.models.music_vae import configs
+from magenta.models.music_vae.trained_model import TrainedModel
+import numpy as np
+import os
+
+def main():
+    print("Music generation script")
+
+if __name__ == "__main__":
+    main()
